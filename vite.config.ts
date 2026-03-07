bd5e967e139437b58dc8611619681baae871111d
Burak Temelkaya <temelkayaburak@gmail.com>
2026-03-07 18:39:15 +0300

    new domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // GitHub Pages için base path ayarı
-  base: '/RollercoinCalculatorWeb/',
+  // Custom domain için base path ayarı
+  base: '/',
   build: {
     target: "es2015",
     // Disable inlining of SVGs as base64 for production builds
